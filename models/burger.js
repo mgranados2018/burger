0c306a6dfacdaa72db0a993487cdf6423b368f2f
firefly <11maguirre.a@gmail.com>
2019-01-27 17:32:36 -0600

    added and edited the handlebars files and js files. This homework is very similar to the cats class activities which I used as guidance.

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,26 @@
 var orm  = require("../config/orm");
 
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+// Requirement: Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+  };
+  
 
+//   this will be exported to the burgers_controller.js
 module.exports = burger
---
+++ public/assets/js/view.js
@@ -0,0 +1,44 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".eat").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEatenBurger = $(this).data("newEatenBurger");
+
+    var newBurgerState = {
+      eaten: newEatenBurger
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState
+    }).then(
+      function() {
+        console.log("added new burger", newEatenBurger);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#brg").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
