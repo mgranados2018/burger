3de5bb088238fd1893babf9204ba02a7145c8403
firefly <11maguirre.a@gmail.com>
2019-01-30 21:06:23 -0600

    adding heroku/jaws info

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,18 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection ({
+    host: 'localhost',
+    user: 'marisolgranados',
+    password: 'dormirte',
+    database: 'burgers_db'
+  })
+}
+
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
