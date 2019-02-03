b38099a911399d46d534a563e3795c6bf707d613
firefly <11maguirre.a@gmail.com>
2019-02-02 20:02:36 -0600

    fixed the handlebars file and cleaned up some of the code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -36,7 +36,7 @@ router.get("/", function(req, res) {
   
     console.log("condition", condition);
   
-    burger.update({
+    burger.updateOne({
       devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -4,11 +4,12 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(document).ready(function() {
   $(".change-burgerstate").on("click", function(event) {
+    event.preventDefault();
+    
     var id = $(this).data("id");
-    var newEatenBurger = $(this).data("newEatenBurger");
-    console.log (newEatenBurger,"neweatenburger")
+
     var newBurgerState = {
-      devoured: newEatenBurger
+      devoured: true
     };
 
     // Send the PUT request.
@@ -17,33 +18,12 @@ $(document).ready(function() {
       data: newBurgerState
     }).then(
       function() {
-        console.log("added new burger", newEatenBurger);
+        console.log("added new burger", newBurgerState);
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  // $(".create-form").on("submit", function(event) {
-  //   // Make sure to preventDefault on a submit event.
-  //   event.preventDefault();
-    
-  //   var newBurger = {
-  //     name: $("#brg").val().trim(),
-  //     devoured: false
-  //   };
 
-    
-  //   // Send the POST request.
-  //   $.ajax("/api/burgers", {
-  //     type: "POST",
-  //     data: newBurger
-  //   }).then(
-  //     function() {
-  //       console.log("created new burger");
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     }
-  //   );
-  // });
 });
